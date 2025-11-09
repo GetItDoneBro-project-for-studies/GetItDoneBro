8a2d42b39e8992629c6ad6c7b8cd944bc0b6ff0e
Wojciech <wojtasiek212@gmail.com>
2025-11-09 18:49:46 +0100

    GDIB-7 feat: Add logout functionality to the App component

--- src/frontend/GetItDoneBro.FrontEnd/src/App.tsx
+++ src/frontend/GetItDoneBro.FrontEnd/src/App.tsx
@@ -1,13 +1,14 @@
 import { KeycloakGuard } from "./components/KeycloakGuard";
 import { useAuth } from "./hooks/useAuth";
 function App() {
-  const { getUserProfile } = useAuth();
+  const { getUserProfile, logout } = useAuth();
   return (
     <KeycloakGuard>
       <p>
         Działa, zalogowano jako:
         <span style={{ fontWeight: 600 }}> {getUserProfile().name}</span>
       </p>
+      <button onClick={logout}>logout</button>
     </KeycloakGuard>
   );
 }

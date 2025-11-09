735b2eedb575dd2a9bea0554befe8f21a3e66426
Dominik Podraza <podraza.dominik@gmail.com>
2025-11-09 20:49:32 +0100

    feat: update Keycloak integration and configuration, remove obsolete files, and enhance authentication setup

--- src/backend/GetItDoneBro.Api/Configurations/DependencyInjection.cs
+++ src/backend/GetItDoneBro.Api/Configurations/DependencyInjection.cs
@@ -1,22 +1,13 @@
 ﻿using GetItDoneBro.Infrastructure;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Sdk;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace GetItDoneBro.Api.Configurations;
 
 public static class DependencyInjection
 {
-    public static WebApplication ConfigureApplication(this WebApplication app)
-    {
-        app.MapDefaultEndpoints();
-        app.UseDefaultFiles();
-        app.UseStaticFiles();
-        app.ApplyMigrations();
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.MapFallbackToFile("/index.html");
-        return app;
-    }
-
     public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
     {
         builder.Host.CustomConfigureAppConfiguration();
@@ -25,8 +16,32 @@ public static class DependencyInjection
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddInfrastructure(builder.Configuration);
+        
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddKeycloakWebApi(builder.Configuration);
+
+        builder.Services
+            .AddAuthorization()
+            .AddAuthorizationBuilder();
+
+        builder.Services
+            .AddKeycloakAuthorization()
+            .AddAuthorizationServer(builder.Configuration);
+        
+        builder.Services.AddAuthorization();
         return builder;
     }
+    public static WebApplication ConfigureApplication(this WebApplication app)
+    {
+        app.MapDefaultEndpoints();
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+        app.ApplyMigrations();
+        app.UseHttpsRedirection().UseAuthentication().UseAuthorization();
+        return app;
+    }
+
     
     
 }
